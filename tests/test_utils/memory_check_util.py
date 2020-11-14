e9190b18e27c490e03fd8913ac0362e77e2d90e9
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2020-11-14 11:12:46 +0100

    q

--- tests/test_utils/memory_check_util.py
+++ tests/test_utils/memory_check_util.py
@@ -52,18 +52,15 @@ async def run_independent_backtestings_with_memory_check(config, tentacles_setup
             # ensure at least one order is either open or got filled
             assert trades + open_orders
             trades = open_orders = exchange_manager = None  # prevent memory leak
-            await asyncio.sleep(0.3)
-            for _ in range(50):
-                await wait_asyncio_next_cycle()
-            await stop_independent_backtesting(backtesting, memory_check=True)
             await asyncio.wait_for(backtesting.post_backtesting_task, 5)
+            await stop_independent_backtesting(backtesting, memory_check=True)
             asyncio.get_event_loop().call_soon(check_independent_backtesting_remaining_objects, backtesting)
             await asyncio.create_task(error_container.check())
     except Exception as e:
         if backtesting is not None:
             # do not get stuck in running backtesting
-            await stop_independent_backtesting(backtesting, memory_check=False)
             await asyncio.wait_for(backtesting.post_backtesting_task, 5)
+            await stop_independent_backtesting(backtesting, memory_check=False)
         raise e
 
 
